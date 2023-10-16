4e7270b1f10a4175b5bc13a81f809b525479d87f
Tari Attila <finyo3549@gmail.com>
2023-10-16 19:52:10 +0200

    Timer class létrehozás

---
+++ Clock/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    internal class Timer
+    {
+        public int remainingSeconds {  get; set; }
+        public Timer(int seconds)
+        {
+            remainingSeconds = seconds;
+        }
+        public void SetTime(int seconds)
+        {
+            remainingSeconds = seconds;
+        }
+        public void StartTimer()
+        {
+            Console.WriteLine("Az időzítő elindult.");
+            for (int i = remainingSeconds; i > 0; i--)
+            {
+                Console.WriteLine($"Hátralévő idő: {i} másodperc");
+                Thread.Sleep(1000); // Várunk 1 másodpercet
+            }
+            Console.WriteLine("Az idő lejárt.");
+        }
+    }
+}
+
